d3058a5c7d8f507c1385e44af852caa56be03293
Ajay <abajad1999@gmail.com>
2025-02-07 18:38:25 +0530

    fisrt commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,96 @@
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
+
+export const LINKS = {
+  github: 'https://github.com/ajaybajad/',
+  linkedin: 'https://www.linkedin.com/in/ajay-bajad/',
+  instagram: 'https://www.instagram.com/',
+}
+
+// Projects Page
+export const PROJECTS = [
+  {
+    "name": "Gate Pass Management",
+    "description": "The Gate pass management project.",
+    "url": "https://github.com/ajaybajad/Gate-Pass-Managemet",
+    "hosted_url": "https://github.com/ajaybajad/Gate-Pass-Managemet",
+    "languages": ["C#", ".Net", "MSSQL"]
+  },
+
+  {
+    "name": "E-Commerce Website",
+    "description": "An E-Commerce website made using HTML, CSS & JS.",
+    "url": "https://ajaybajad.github.io/ecommerce/",
+    "hosted_url": "https://ajaybajad.github.io/ecommerce/",
+    "languages": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "name": "Personal Portfolio",
+    "description": "A personal developer portfolio created as a single page portfolio made as a bento grid.",
+    "url": "https://github.com/ajaybajad/Personal-Portfolio",
+    "hosted_url": "https://vercel.com/",
+    "languages": ["JavaScript", "Typescript", "React", "Astro", "CSS"]
+  },
+  {
+    "name": "Password Generator",
+    "description": "This is password Generator made using HTML,CSS & JS.",
+    "url": "https://ajaybajad.github.io/password-generator/",
+    "hosted_url": "https://ajaybajad.github.io/password-generator/",
+    "languages": ["HTML", "CSS", "JavaScript"]
+  }
+]
+
+// Education Page
+export const EDUCATION = [
+  {
+    name: "MGMS's Dr. G. Y. Pathrikar College OFCs & IT",
+    location: 'Samhajinagar (Aurangabad), MH, India',
+    position: 'Bachelor of Computer Applications',
+    start: '2020',
+    link: 'https://mgmu.ac.in/',
+    end: '2023',
+    tasks: [
+    ],
+  },
+  {
+    name: 'Arts & Science College',
+    location: 'Mangrul Navghare, MH, India',
+    position: 'Higher Secondary Education',
+    link: '',
+    start: '2016',
+    end: '2027',
+    tasks: [
+    ],
+  },
+]
+
+// Experience Page
+export const EXPERIENCE = [
+  {
+    company: 'Modi Innovations Pvt Ltd',
+    position: 'Software Developer Intern', 
+    start: 'Jan 2023',
+    end: 'Mar 2023',
+    link: 'https://www.modiinnovations.com/',
+    tasks: [
+      'Developed and maintained scalable software applications using C# .NET, MS SQL.',
+      'Optimized code performance, reducing code duplication.',
+      'Enhanced application security and performance.'
+    ],
+  },
+  {
+    company: 'Internpe',
+    position: 'Web Developer Intern',
+    start: 'Nov 2024',
+    end: 'Dec 2024',
+    link: 'https://internpe.in/',
+    tasks: [
+      'Designed and developed responsive web pages using HTML, CSS, and JavaScript, ensuring cross-browser compatibility.',
+      'Improved web applications interactivity and functionality through JavaScript logic and dynamic elements.',
+      'Applied logical problem-solving skills to enhance project design and usability.'
+    ],
+  },
+]
